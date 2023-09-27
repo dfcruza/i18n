d44bb2921ef1584ab7fc1d300b35cc438a8bd747
dfcruza <60239198+dfcruza@users.noreply.github.com>
2023-09-26 23:09:19 -0500

    2/6 retos completos

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Job from "./job";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 
 const JobsList = () => {
   const [offers] = useState([
@@ -11,6 +11,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      views: 12500000000000000000,
     },
     {
       id: "0002",
@@ -19,6 +20,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      views: 15000000000000000000000,
     },
     {
       id: "0003",
@@ -27,6 +29,7 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      views: 1000000000000000000,
     },
   ]);
 
@@ -35,14 +38,25 @@ const JobsList = () => {
       <table className="table">
         <thead className="thead-dark">
           <tr>
+            <th scope="col">#</th>
             <th scope="col">
               <FormattedMessage id="Position" />
             </th>
-            <th scope="col">Position</th>
-            <th scope="col">Company</th>
-            <th scope="col">Salary</th>
-            <th scope="col">City</th>
-            <th scope="col">Publication date</th>
+            <th scope="col">
+              <FormattedMessage id="Company" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Salary" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="City" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Publication date" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="views" />
+            </th>
           </tr>
         </thead>
         <tbody>
